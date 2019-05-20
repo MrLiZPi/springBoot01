4d161ffeaf24b7e4a7716ac14ed5bd9f10cc9dab
mzy <cn_ss_mzy@qq.com>
2019-05-20 14:56:38 +0800

    first SpringBoot

---
+++ src/main/java/com/springBoot/HelloWorldApplication.java
@@ -0,0 +1,15 @@
+package com.springBoot;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author shkstart
+ * @create 2019-05-19-19:52
+ */
+@SpringBootApplication
+public class HelloWorldApplication {
+    public static void main(String[] args) {
+
+    }
+}
